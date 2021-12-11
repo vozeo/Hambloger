4db10a157d312a2f6087fb428ae70a15a688962a
Vozeo <2488684796@qq.com>
2021-12-11 21:24:30 +0800

    Fix edit post page.

--- app/main/errors.py
+++ app/main/errors.py
@@ -4,14 +4,14 @@ from . import main
 
 @main.app_errorhandler(403)
 def prohibition(e):
-    return render_template('403.html'), 403
+    return render_template('403.html', edit_post=edit_post), 403
 
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('404.html', edit_post=edit_post), 404
 
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template('500.html', edit_post=edit_post), 500
